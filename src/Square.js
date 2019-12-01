c6118541d64c369734fa25742f135127b1fe9003
Yulia Gurevich <yulia.v.gurevich@gmail.com>
2019-12-01 22:11:28 +0200

    Implement tic-tac-toe game.

---
+++ src/Square.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={() => { props.onClick() }}
+        >
+            {props.value}
+        </button>
+    );
+}
+
+export default Square;
